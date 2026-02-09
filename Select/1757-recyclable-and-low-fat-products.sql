02be2453b1b596903f4506334c82418d8e1367b3
JihyeJayPark <jihyepark1478@gmail.com>
2026-02-09 08:47:39 -0500

    feat: modify the template

--- Select/1757-recyclable-and-low-fat-products.sql
+++ Select/1757-recyclable-and-low-fat-products.sql
@@ -9,7 +9,7 @@ Write a solution to find the ids of products that are both low fat and recyclabl
 Return the result table in any order.
 */
 
--- Your Solution:
+-- My Solution:
 SELECT 
     product_id
 FROM 
